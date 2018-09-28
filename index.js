58981caf958d8294b6392e0bbc88402d878e95de
Nate Miller <natekmiller96@gmail.com>
2018-09-28 14:51:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function findMatching(drivers, string){
   
-const result = drivers.filter(word => word === string);
+const result = drivers.filter(word => word.toLowerCase() === string.toLowerCase());
 
 return result;
 }
