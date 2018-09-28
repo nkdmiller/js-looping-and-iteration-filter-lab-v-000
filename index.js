c92eba3a1a78d0f77a83c3fdfcbe928f8ecc36ee
Nate Miller <natekmiller96@gmail.com>
2018-09-28 14:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const result = drivers.filter(word => word.toLowerCase() === string.toLowerCase(
 
 return result;
 }
+
+function fuzzy(drivers, string){
+  for 
+}
+function fuzzyMatch(drivers, string){
+  const result = drivers.filter(word =>)
+}
