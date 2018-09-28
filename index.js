2fb9ddd906d2bb5bc7f07203b2ac5486e18c8b6b
Nate Miller <natekmiller96@gmail.com>
2018-09-28 15:05:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ return result;
 
 
 function fuzzyMatch(drivers, string){
-  const result = drivers.filter(word => word.slice[0, string.length-1] === string);
+  const slength = string.length - 1;
+  const slicedDrivers = [];
+  for (driver in)
+  const result = drivers.filter(word => word === string);
   return result;
 }
